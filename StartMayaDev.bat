b6141946eba3fb4ae36fe5890f99a037caf31ba7
Muream <muream@gmail.com>
2019-07-01 15:25:46 +0200

    Add User and Dev Batch files

--- StartMayaDev.bat
+++ StartMayaDev.bat
@@ -3,8 +3,6 @@
 set ToolsPath=%cd%
 set MAYA_MODULE_PATH=%ToolsPath%
 
-rem git pull
-
 for /f "tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Autodesk\Maya\2019\Setup\InstallPath" /v MAYA_INSTALL_LOCATION') do set "MayaInstallDir=%%~b"
 cd /d %MayaInstallDir%
 start bin\maya.exe
